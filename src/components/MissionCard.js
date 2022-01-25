79d0e8cd4eaf50bebea3e93d1d468f1047ec926a
Eduardo Sousa <eduardobrunoit@gmail.com>
2022-01-25 17:10:48 -0300

    Requisito 09 - Corrigido

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -31,7 +31,7 @@ export default MissionCard;
 
 MissionCard.propTypes = {
   name: PropTypes.string.isRequired,
-  year: PropTypes.number.isRequired,
+  year: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
   destination: PropTypes.string.isRequired,
 };
